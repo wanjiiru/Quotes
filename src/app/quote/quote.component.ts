6eef96614e7e891980d1f6946cbf6f6cc4589f30
wanjiiru <liz240@live.com>
2018-05-25 16:20:06 +0300

    add form styles

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,8 +10,9 @@ import {Quote} from '../quote'
 export class QuoteComponent implements OnInit {
   title = "QUOTES!"
   quotes =[
-    new Quote(1, 'Mandera','Liz','hello world',new Date(2018,5,25,) ),
-    new Quote(1, 'Mandera','Liz','hello world',new Date(2018,5,25,) )
+    new Quote(1, 'Mandera','Liz','Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do'
+    ,new Date(2018,5,25,) ),
+    new Quote(1, 'Mandera','Liz','  Challenges are what make life interesting and overcoming them is what makes life meaningful',new Date(2018,5,25,) )
 
   ]
 
@@ -24,10 +25,6 @@ addNewQuote(quote){
   this.quotes.push(quote)
 }
 
-
-
-
-
   completeQuote(isComplete,index){
     if(isComplete){
       let toComplete = confirm('Are you sure you want to delete?')
@@ -36,9 +33,6 @@ addNewQuote(quote){
       }
     }
   }
-
-
-
 
 
 
